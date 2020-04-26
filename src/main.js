05bed02b4a8d51479f6e5598fd9f9c517943809c
gitzky <41283544+gitzky@users.noreply.github.com>
2020-04-26 19:21:52 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,12 +1,45 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+// 全局引入echarts  也可以在组件中按需引入
+// import echarts from "echarts";
+// Vue.prototype.$echarts = echarts;
+
+// 引入element-ui
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+Vue.use(ElementUI);
+
+import VueAMap from "vue-amap";
+Vue.use(VueAMap);
+VueAMap.initAMapApiLoader({
+  key: "your amap key",
+  plugin: [
+    "AMap.Autocomplete",
+    "AMap.PlaceSearch",
+    "AMap.Scale",
+    "AMap.OverView",
+    "AMap.ToolBar",
+    "AMap.MapType",
+    "AMap.PolyEditor",
+    "AMap.CircleEditor"
+  ],
+  // 默认高德 sdk 版本为 1.4.4
+  v: "1.4.4"
+});
+
+import "./assets/css/index.scss";
+import "./assets/fonts/iconfont.css";
+import "./assets/fonts/syst.css";
 
 new Vue({
   router,
   store,
-  render: function (h) { return h(App) }
-}).$mount('#app')
+  render: function(h) {
+    return h(App);
+  }
+}).$mount("#app");
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <el-button>添加一个按钮</el-button>
+    <el-form>
+      <el-form-item label="随便一个输入框">
+        <el-input></el-input>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
